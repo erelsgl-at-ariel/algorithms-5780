4d5f9ec1fe9d0bd6ed6069fb7cd463f0c75605df
Erel Segal-Halevi cron <erelsgl@gmail.com>
2019-11-27 16:46:22 +0200

    Add solutions to hw 4

--- 02-pareto-efficiency/code/cvxpy-demo.py
+++ 02-pareto-efficiency/code/cvxpy-demo.py
@@ -4,21 +4,21 @@
 A demo of cvxpy - the convex-optimization package of python.
 """
 
-import cvxpy as cp
+import cvxpy
 
 # Create two scalar optimization variables.
-x = cp.Variable()
-y = cp.Variable()
+x = cvxpy.Variable()
+y = cvxpy.Variable()
 
 # Create two constraints.
 constraints = [x + y == 1,
                x - y >= 1]
 
 # Form objective.
-obj = cp.Minimize((x - y)**2)
+obj = cvxpy.Minimize((x - y)**2)
 
 # Form and solve problem.
-prob = cp.Problem(obj, constraints)
+prob = cvxpy.Problem(obj, constraints)
 prob.solve()  # Returns the optimal value.
 print("status:", prob.status)
 print("optimal value", prob.value)
