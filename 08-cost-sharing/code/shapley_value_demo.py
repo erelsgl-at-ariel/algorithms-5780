ddd198060a0f2790b88d3835c1d1486195b7e044
Erel Segal-Halevi cron <erelsgl@gmail.com>
2019-12-14 22:00:24 +0200

    New slides, code for Shapley value, and airport problem

---
+++ 08-cost-sharing/code/shapley_value_demo.py
@@ -0,0 +1,47 @@
+#!python3
+
+"""
+Demo of Shapley value calculation.
+
+Programmer: Erel Segal-Halevi
+Since: 2019-12
+"""
+
+import itertools, collections
+from dicttools import stringify  # Install dicttools from here: https://github.com/trzemecki/dicttools
+
+import logging
+logger = logging.getLogger(__name__)
+
+import shapley_value
+import logging, sys
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.INFO)
+
+
+print("\n## Travel cost sharing example")
+map_subset_to_cost = {
+	"":	   0,
+	"a":   5,
+	"b":   9,
+	"ab":  11,
+}
+sv = shapley_value.shapley_values("ab", map_subset_to_cost)
+for player,value in sv.items():
+	print("Shapley value of {} is {}".format(player, value))
+
+
+print("\n## Airport problem example")
+map_subset_to_cost = {
+	"":	   0,
+	"a":   3,
+	"b":   23,
+	"c":   123,
+	"ab":  23,
+	"ac":  123,
+	"bc":  123,
+	"abc": 123,
+}
+sv = shapley_value.shapley_values("abc", map_subset_to_cost)
+for player in sorted(sv.keys()):
+	print("Shapley value of {} is {}".format(player, sv[player]))
