ff4e2528ad50875c19382a717efcc4174441cb45
Erel Segal-Halevi cron <erelsgl@gmail.com>
2020-01-27 16:24:52 +0200

    solutions week 12

--- 02-pareto-efficiency/code/maximize-product-2.py
+++ 02-pareto-efficiency/code/maximize-product-2.py
@@ -12,23 +12,20 @@ import math, cvxpy
 from cvxpy import log
 
 print("\n\n\nPROBLEM #1")
-print("A cake with three regions has to be divided among 2 people with values:")
-print("2 3 4")
-print("8 7 6")
+print("A cake with two regions has to be divided among 2 people with values:")
+print("2 2")
+print("0 4")
 
 # Define x,y,z = the fraction of each region given to player 1.
-x = cvxpy.Variable()
-y = cvxpy.Variable()
-z = cvxpy.Variable()
+x, y, z = cvxpy.Variable(3)
 
 print("\nMaximize the sum of logs:")
 prob = cvxpy.Problem(
-    objective   =  cvxpy.Maximize(log(2*x + 3*y + 4*z) + log(8*(1-x)+7*(1-y)+6*(1-z))),
-    constraints = [0 <= x, x <= 1, 0 <= y, y <= 1, 0 <= z, z <= 1])
+    objective   =  cvxpy.Maximize(log(2*x + 4*y) + log(0*(1-x)+4*(1-y))),
+    constraints = [0 <= x, x <= 1, 0 <= y, y <= 1])
 prob.solve()
 print("status:", prob.status)
 print("optimal value", prob.value)
 print("optimal product", math.exp(prob.value))
 print("optimal x", x.value)
 print("optimal y", y.value)
-print("optimal z", z.value)
